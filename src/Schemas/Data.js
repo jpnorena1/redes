84216e554d0faa2294811368f894a59aa0f51c5c
jpnorena1 <73927801+jpnorena1@users.noreply.github.com>
2024-05-19 18:44:40 -0500

    test

---
+++ src/Schemas/Data.js
@@ -0,0 +1,100 @@
+export const Data = {
+  profile: {
+    name: "Juan Pablo Noreña Aguirre",
+    ocupation: "FullStack Software Developer",
+    location: "Bogota, Colombia",
+    email: "mcna1013@gmail.com",
+    telephone: "+57 3134332673",
+    image: "images/me.webp",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "Analista Programador, capacitado para desarrollar aplicaciones informáticas en diversas tecnologías y múltiples lenguajes de programación, aplicando metodologías ágiles de desarrollo, incluyendo identificación de necesidades y diseños de sistemas basados en modelos de desarrollo.",
+  },
+  skills: {
+    technicalLabel: "Tecnologías",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Angular",
+      "React",
+      "Java",
+      "Nodejs",
+      "Python",
+      "Docker",
+      "Aws",
+      "SQL",
+      "Git",
+      "NoSQL",
+      "Serverless",
+      "PWA"
+    ],
+    softSkills: [
+      "Capacidad de análisis",
+      "Resolución de Problemas",
+      "Pensamiento Crítico",
+      "Trabajo en equipo",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta",
+      "Mentoria",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visitar el perfil de Matias Lagos en LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/juan-pablo-nore%C3%B1a-aguirre-4875a4213/",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Visitar el perfil de Juan Noreña en GitHub",
+        name: "github",
+        url: "https://github.com/jpnorena1",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "febero 2024  - Actualidad",
+        company: "Digital Ware Advisory",
+        description: [
+          "Involucrado en el despliegue de servicios en AWS EC2 para 5 proyectos, abarcando tanto el frontend como el backend, Agentes d IA, contenerización en imágenes Docker y despliegue continuo con Jenkins. Utilicé GitHub para el almacenamiento de repositorios y Docker Compose para la gestión de bases de datos. Opté por desplegar los servicios en la región de Norte de Virginia de AWS, optimizando la latencia y diferenciación de costos"
+        ],
+      },
+      {
+        title: "Desarrollador Serverless",
+        period: "Enero. 2023 - Dec 2023",
+        company: "Nequi Colombia",
+        description: [
+          "Cliente Bancario (NeoBanco) Logré implementar script que conectado por medio de módulos facilitaba el  uso de trabajar con servicios Bancarios realizados en lambdas AWS, Actualización de servicios lambdas desde  el lado de backend con tecnología Node.js y Java, Realizar Pruebas de software, Uso de técnicas de código  limpio, Integración y realización de una API con arquitectura REST para consultas. •Desarrollador Nodejs •Implementaciòn de microservicios •Lambda Functions •Test Code • DynamoDb •S3 •SNS •Jenkins •Sona",
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Ingenieria de sistemas",
+        date: "2017",
+        institution: "Universidad El Bosque",
+      },
+    ],
+    proyects: [
+      {
+        name: "Octav-IA",
+        company: "BPO PFIZER",
+        period: "Nov. 2022 - Ene. 2023",
+        description: [
+          "Control, desarrollo, securitización y despliegue de APIs en plataforma AWS API Gateway.",
+        ],
+      },
+      
+      
+    ],
+  },
+};
